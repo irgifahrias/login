931de1750852946664240a117bc7e62a0e9e5854
aryafirman <aryafirman40@gmail.com>
2019-01-18 11:47:14 +0700

    admin

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,10 +12,17 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         \App\User::create([
-            'name' => "irgi fahrias",
+            'name' => "Arya Firman",
+            'email' => "aryafirman40@gmail.com",
+            'admin' => 1,
+            'password' => bcrypt('qwerty23')
+        ]);
+
+        \App\User::create([
+            'name' => "Irgi Fahrias",
             'email' => "fahrias600@gmail.com",
             'admin' => 1,
-            'password' => bcrypt('irgi551')
+            'password' => bcrypt('123456')
         ]);
     }
 }
